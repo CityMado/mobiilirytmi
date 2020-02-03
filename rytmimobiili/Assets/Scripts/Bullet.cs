ace500fb72feb961e5a8363be38fec92b2adb5bc
CityMado <stenius.jere@gmail.com>
2020-02-03 22:36:39 +0200

    Player can now spawn bullets

---
+++ rytmimobiili/Assets/Scripts/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float Speed = 10;
+
+
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = transform.up * Speed;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- rytmimobiili/Assets/Scripts/PlayerController.cs
+++ rytmimobiili/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Transform Cannon;
+    public GameObject BulletPrefab;
+   // public float bulletDistance;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+            Debug.Log("pressed");
+        }
     }
-}
+      private void Shoot()
+        {
+        var bullet = Instantiate(BulletPrefab);
+            bullet.transform.rotation = transform.rotation;
+        bullet.transform.position = transform.position + transform.up;
+            Debug.Log("fired");
+        }
+            
+    }
